ed5bb5cdae762b9ea4fbc09fd17aba141ce72b32
Ritesh Vishwakarma <ritesh@Riteshs-MacBook-Pro.local>
2019-02-17 14:44:16 +0530

    role done

--- common/models/users.js
+++ common/models/users.js
@@ -1,6 +1,5 @@
 'use strict'
-let counter=0;
-let app = require('../../server/server');
+
 module.exports = function (Users) {
   const roles = []
   Users.validatesInclusionOf('role', { 'in': roles })
@@ -13,25 +12,11 @@ module.exports = function (Users) {
       next()
     })
   })
-/*
-db.Users.remove({})
-db.RoleMapping.remove({})
 
-    //  Users.getDataSource().connector.connect(function (err, db) {
-    //        var collection = db.collection('Users'); //name of db collection
-    //        console.log(Object.keys(Users.app.models));
-    //        collection.find(function(err, res){
-    //          res.toArray(function(err, realRes){
-    //            console.log(realRes);
-    //          });
-    //        });
-    // });
-
-*/
-Users.observe('after save', function event (ctx, next) {
+  Users.observe('after save', function event (ctx, next) {
     const { Role, RoleMapping } = Users.app.models
-    const role=(ctx.instance.role).toString()
-    Role.findOne({where: {name:role}} , (err, data) => {
+    const role = (ctx.instance.role).toString()
+    Role.findOne({ where: { name: role } }, (err, data) => {
       if (err) { return next(err) }
       data.principals.create({
         principalType: RoleMapping.USER,
