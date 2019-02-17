13128c95a5611cef3af00ba5f711fcb1f19e50c2
Ritesh Vishwakarma <ritesh@Riteshs-MacBook-Pro.local>
2019-02-17 14:29:43 +0530

    role done

--- common/models/users.js
+++ common/models/users.js
@@ -1,9 +1,9 @@
 'use strict'
 let counter=0;
+let app = require('../../server/server');
 module.exports = function (Users) {
   const roles = []
   Users.validatesInclusionOf('role', { 'in': roles })
-
 
   Users.observe('before save', function event (ctx, next) {
     const { Role } = Users.app.models
@@ -12,23 +12,10 @@ module.exports = function (Users) {
       for (var i = 0; i < data.length; i++) { roles.push(data[i]['name']) }
       next()
     })
-
-    Users.observe('after save', function event (ctx, next) {
-      const { Role, RoleMapping } = Users.app.models
-      Role.findOne({ name: ctx.instance.role }, (err, data) => {
-        console.log("here",counter++);
-        if (err) { return next(err) }
-        data.principals.create({
-          principalType: RoleMapping.USER,
-          principalId: ctx.instance.id
-        }, function (err, principal) {
-          if (err) { return next(err) }
-          next()
-        })
-      })
-    })
-
-    // console.log(dataSource);
+  })
+/*
+db.Users.remove({})
+db.RoleMapping.remove({})
 
     //  Users.getDataSource().connector.connect(function (err, db) {
     //        var collection = db.collection('Users'); //name of db collection
@@ -40,15 +27,19 @@ module.exports = function (Users) {
     //        });
     // });
 
-    // Observe any insert/update event on Model
-    // if (ctx.instance) {
-    //   ctx.instance.createdAt = new Date()
-    //   ctx.instance.createdBy = ctx.options.accessToken.userId
-    //   ctx.instance.updatedAt = new Date()
-    //   ctx.instance.updatedBy = ctx.options.accessToken.userId
-    // } else {
-    //   ctx.data.updatedAt = new Date()
-    //   ctx.data.updatedBy = ctx.options.accessToken.userId
-    // }
+*/
+Users.observe('after save', function event (ctx, next) {
+    const { Role, RoleMapping } = Users.app.models
+    const role=(ctx.instance.role).toString()
+    Role.findOne({where: {name:role}} , (err, data) => {
+      if (err) { return next(err) }
+      data.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: ctx.instance.id
+      }, function (err, principal) {
+        if (err) { return next(err) }
+        next()
+      })
+    })
   })
 }
